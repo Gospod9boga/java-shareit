a6822fff60102c1bef4fbef58129c50175bce5bf
Gospod9boga <didovecv@mail.ru>
2025-12-25 10:43:45 +0300

    изменен AppConfig

--- gateway/src/main/java/ru/practicum/gateway/AppConfig.java
+++ gateway/src/main/java/ru/practicum/gateway/AppConfig.java
@@ -3,17 +3,17 @@ package ru.practicum.gateway;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import java.time.Duration;
 
 @Configuration
 public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-
         return builder
-                .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(10))
                 .build();
     }
 }
